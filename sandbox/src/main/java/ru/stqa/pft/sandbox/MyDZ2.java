8ec5b37ad734b9e834c984ec4ff2b3f661af8ee9
Черная Лариса <lchernaya@masterdm.ru>
2020-10-27 01:27:50 +0300

    ДЗ_2 4. Функция distance переделана в метод

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyDZ2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyDZ2.java
@@ -8,18 +8,11 @@ package ru.stqa.pft.sandbox;
 public class MyDZ2 {
 
    public static void main(String[] args) {
-       Point p1 = new Point();
-      Point p2 = new Point();
-      p1.x = 1;
-      p1.y =2;
-      p2.x = 4;
-      p2.y =6;
-      System.out.println("Расстояние между точками ("+p1.x+", "+p1.y+") и (" +p2.x+", "+p2.y + ") = "+distance(p1, p2));
+      Point p1 = new Point(1, 2);
+      Point p2 = new Point(4, 6);
+      System.out.println("Расстояние между точками (" + p1.x + ", " + p1.y + ") и (" + p2.x + ", " + p2.y + ") = " + p1.distance(p2));
    }
 
-   public static double distance(Point p1, Point p2) {
-      return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
-   }
-   }
+}
 
 
