db873d71640321258592ab094a7403fe7c0a5c70
Черная Лариса <lchernaya@masterdm.ru>
2020-11-01 22:05:38 +0300

    ДЗ_4 добавление класса UserData

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTests.java
@@ -30,64 +30,64 @@ public class UserCreationTests {
   @Test
   public void testUserCreation() throws Exception {
     initUserCreation();
-    fillUserForm("Антоний", "Васильевич", "Погорелов", "Domino", "Ant", "LTD", "Москва, ул.Нагорная 7", "8495555555555", "+7910555555555",
-                 "test@mail.ru", "7", "November", "1991", "Москва, Сосновая ул. 3-205", "Новый пользователь");
+    fillUserForm(
+         new UserData("Антоний", "Васильевич", "Погорелов", "Domino", "Ant", "LTD", "Москва, ул.Нагорная 7", "8495555555555", "+7910555555555",
+                      "test@mail.ru", "7", "November", "1991", "Москва, Сосновая ул. 3-205", "Новый пользователь"));
     submitUserCreation();
     gotoHomePage();
     logout();
   }
 
-  private void fillUserForm(String firstname, String middlename, String lastname, String title, String nickname, String company, String address,
-                            String homephone, String mobile, String email, String bday, String bmonth, String byear, String address2, String notes) {
+  private void fillUserForm(UserData userData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(firstname);
+    wd.findElement(By.name("firstname")).sendKeys(userData.getFirstname());
     wd.findElement(By.name("middlename")).click();
     wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(middlename);
+    wd.findElement(By.name("middlename")).sendKeys(userData.getMiddlename());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(lastname);
+    wd.findElement(By.name("lastname")).sendKeys(userData.getLastname());
     wd.findElement(By.name("nickname")).click();
     wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(nickname);
+    wd.findElement(By.name("nickname")).sendKeys(userData.getNickname());
     wd.findElement(By.name("title")).click();
     wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys(title);
+    wd.findElement(By.name("title")).sendKeys(userData.getTitle());
     wd.findElement(By.name("company")).click();
     wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(company);
+    wd.findElement(By.name("company")).sendKeys(userData.getCompany());
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(address);
+    wd.findElement(By.name("address")).sendKeys(userData.getAddress());
 
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(homephone);
+    wd.findElement(By.name("home")).sendKeys(userData.getHomephone());
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(mobile);
+    wd.findElement(By.name("mobile")).sendKeys(userData.getMobile());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(email);
+    wd.findElement(By.name("email")).sendKeys(userData.getEmail());
     wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(bday);
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(userData.getBday());
     wd.findElement(By.xpath("//option[@value='7']")).click();
     wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(bmonth);
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(userData.getBmonth());
     wd.findElement(By.xpath("//option[@value='November']")).click();
     wd.findElement(By.name("byear")).click();
     wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(byear);
+    wd.findElement(By.name("byear")).sendKeys(userData.getByear());
     wd.findElement(By.xpath("(//option[@value='2'])[3]")).click();
 
     wd.findElement(By.name("address2")).click();
     wd.findElement(By.name("address2")).clear();
-    wd.findElement(By.name("address2")).sendKeys(address2);
+    wd.findElement(By.name("address2")).sendKeys(userData.getAddress2());
     wd.findElement(By.name("notes")).click();
     wd.findElement(By.name("notes")).clear();
-    wd.findElement(By.name("notes")).sendKeys(notes);
+    wd.findElement(By.name("notes")).sendKeys(userData.getNotes());
   }
 
   private void initUserCreation() {
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserData.java
@@ -0,0 +1,174 @@
+package ru.stqa.pft.addressbook;
+
+public class UserData {
+
+   private final String firstname;
+   private final String middlename;
+   private final String lastname;
+   private final String title;
+   private final String nickname;
+   private final String company;
+   private final String address;
+   private final String homephone;
+   private final String mobile;
+   private final String email;
+   private final String bday;
+   private final String bmonth;
+   private final String byear;
+   private final String address2;
+   private final String notes;
+
+   public UserData(String firstname, String middlename, String lastname, String title, String nickname, String company, String address,
+                   String homephone, String mobile, String email, String bday, String bmonth, String byear, String address2, String notes) {
+      this.firstname = firstname;
+      this.middlename = middlename;
+      this.lastname = lastname;
+      this.title = title;
+      this.nickname = nickname;
+      this.company = company;
+      this.address = address;
+      this.homephone = homephone;
+      this.mobile = mobile;
+      this.email = email;
+      this.bday = bday;
+      this.bmonth = bmonth;
+      this.byear = byear;
+      this.address2 = address2;
+      this.notes = notes;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getFirstname() {
+      return firstname;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getMiddlename() {
+      return middlename;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getLastname() {
+      return lastname;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getTitle() {
+      return title;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getNickname() {
+      return nickname;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getCompany() {
+      return company;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getAddress() {
+      return address;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getHomephone() {
+      return homephone;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getMobile() {
+      return mobile;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getEmail() {
+      return email;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getBday() {
+      return bday;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getBmonth() {
+      return bmonth;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getByear() {
+      return byear;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getAddress2() {
+      return address2;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getNotes() {
+      return notes;
+   }
+}
