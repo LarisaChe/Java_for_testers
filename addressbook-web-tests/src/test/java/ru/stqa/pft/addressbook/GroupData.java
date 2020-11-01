ae02ffd9d463afdb36d0ccd638bea271b18b747a
Черная Лариса <lchernaya@masterdm.ru>
2020-11-01 18:20:06 +0300

    Изменения по лекции 2.4: рефакторинг

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupData.java
@@ -0,0 +1,41 @@
+package ru.stqa.pft.addressbook;
+
+public class GroupData {
+
+   private final String name;
+   private final String header;
+   private final String footer;
+
+   public GroupData(String name, String header, String footer) {
+      this.name = name;
+      this.header = header;
+      this.footer = footer;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getName() {
+      return name;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getHeader() {
+      return header;
+   }
+
+   /**
+    * Returns .
+    *
+    * @return
+    */
+   public String getFooter() {
+      return footer;
+   }
+}
