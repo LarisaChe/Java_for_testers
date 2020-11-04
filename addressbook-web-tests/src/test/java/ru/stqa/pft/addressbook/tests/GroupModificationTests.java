434d06902c909a2dae06be70abd7c19e06b44613
Черная Лариса <lchernaya@masterdm.ru>
2020-11-04 11:54:47 +0300

    Лекция 2.9. GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Description.
+ *
+ * @author lchernaya
+ */
+public class GroupModificationTests extends TestBase {
+
+   @Test
+   public void testGroupModification() {
+      app.getNavigationHelper().gotoGroupPage();
+      app.getGroupHelper().selectGroup();
+      app.getGroupHelper().initGroupModification();
+      app.getGroupHelper().fillGroupForm(new GroupData("Test Modif1", "Test header Modif1", "Test footer Modif1"));
+      app.getGroupHelper().submitGroupModification();
+      app.getGroupHelper().returnToGroupPage();
+   }
+
+}
