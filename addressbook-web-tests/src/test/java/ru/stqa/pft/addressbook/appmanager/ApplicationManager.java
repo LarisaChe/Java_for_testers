339ca7e186ae665383caee586298d385d201f9bb
Черная Лариса <lchernaya@masterdm.ru>
2020-11-06 23:48:26 +0300

    Лекция 3.3. Разные браузеры

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -4,7 +4,10 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 /**
  * Description.
@@ -18,10 +21,20 @@ public class ApplicationManager  {
    private NavigationHelper navigationHelper;
    private GroupHelper groupHelper;
    private WebDriver wd;
+   private String browser;
+
+   public ApplicationManager(String browser) {
+      this.browser = browser;
+   }
 
    public void init() {
-
-      wd = new FirefoxDriver();
+     if (browser == BrowserType.FIREFOX) {
+        wd = new FirefoxDriver();
+     } else if (browser == BrowserType.CHROME) {
+        wd = new ChromeDriver();
+     } else if (browser == BrowserType.IE) {
+        wd = new InternetExplorerDriver();
+     }
       wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
       wd.get("http://localhost/addressbook/index.php");
       groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,7 +17,7 @@ import ru.stqa.pft.addressbook.model.ContactData;
  */
 public class TestBase  {
 
-   protected final ApplicationManager app = new ApplicationManager();
+   protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME); //BrowserType.FIREFOX); //BrowserType.IE
 
    @BeforeMethod(alwaysRun = true)
    public void setUp() throws Exception {
