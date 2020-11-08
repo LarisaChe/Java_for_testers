a57b17ce43826b870a79717fcfa192ccef892934
Черная Лариса <lchernaya@masterdm.ru>
2020-11-08 10:29:09 +0300

    Лекция 3.10. Проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -2,14 +2,19 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 
+import ru.stqa.pft.addressbook.model.GroupData;
+
 public class GroupDeletionTests extends TestBase {
 
-  @Test
-  public void testGroupDeletion() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().selectGroup();
-    app.getGroupHelper().deleteSelectedGroup();
-    app.getGroupHelper().returnToGroupPage();
-  }
+   @Test
+   public void testGroupDeletion() throws Exception {
+      app.getNavigationHelper().gotoGroupPage();
+      if (!app.getGroupHelper().isThereAGroup()) {
+         app.getGroupHelper().createGroup(new GroupData("Test D", "Test header D", "Test footer D"));
+      }
+      app.getGroupHelper().selectGroup();
+      app.getGroupHelper().deleteSelectedGroup();
+      app.getGroupHelper().returnToGroupPage();
+   }
 
 }
