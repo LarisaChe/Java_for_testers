4b754953f88a01902acc2cb7d5a45bb11a14f6e9
Черная Лариса <lchernaya@masterdm.ru>
2020-11-08 11:21:00 +0300

    ДЗ_8 Модификация контакта с проверкой и предусловием

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 /**
  * Description.
@@ -13,6 +14,16 @@ public class ContactModificationTests extends TestBase {
 
    @Test
    public void testContactModification1() {
+      app.getNavigationHelper().gotoHomePage();
+      if (! app.getContactHelper().isThereAContact()) {
+         app.getNavigationHelper().gotoGroupPage();
+         app.getGroupHelper().createGroup(new GroupData("Test_M", "TestHeader M", "TestFooter M"));
+         app.getNavigationHelper().gotoHomePage();
+         app.getContactHelper().createContact(new ContactData("Contact test m", "test m", "test m", null, null,
+                                                              null, null, null, null, null, "5", "May",
+                                                              "2000", null, null, "Test_M"), true);
+         app.getNavigationHelper().gotoHomePage();
+      }
       app.getContactHelper().initContactModification();
       app.getContactHelper().fillContactForm(
            new ContactData("Антоний", "Алексеевич", "Погорельский", "Black hen", "Ant", "LTD", "Москва, Нагорный проезд 9", "8495555555557", "+7910555555557",
@@ -23,6 +34,7 @@ public class ContactModificationTests extends TestBase {
 
    @Test
    public void testContactModification2() {
+      app.getNavigationHelper().gotoHomePage();
       app.getContactHelper().viewContactDetails();
       app.getContactHelper().initContactModificationInView();
       app.getContactHelper().fillContactForm(
@@ -34,6 +46,7 @@ public class ContactModificationTests extends TestBase {
 
    @Test
    public void testAddContactToGroup() {
+      app.getNavigationHelper().gotoHomePage();
       app.getContactHelper().selectContact();
       app.getContactHelper().addContactToGroup();
       app.getNavigationHelper().gotoHomePage();
