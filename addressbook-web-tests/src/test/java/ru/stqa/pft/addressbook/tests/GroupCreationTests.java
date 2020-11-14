d4ef376ebaee5e38edbbaf90bd68e62026dc7962
Черная Лариса <lchernaya@masterdm.ru>
2020-11-15 00:19:00 +0300

    Лекция 4.8. Поиск максимального элемента в коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import java.util.HashSet;
 import java.util.List;
 
 import org.testng.Assert;
@@ -9,16 +10,28 @@ import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
+   @Test
+   public void testGroupCreation() throws Exception {
+      app.getNavigationHelper().gotoGroupPage();
 
-    List<GroupData> before = app.getGroupHelper().getGroupList();
-    //app.getGroupHelper().createGroup(new GroupData("Test 1", "Test header 1", "Test footer 1"));
-    app.getGroupHelper().createGroup(new GroupData("Test 1", null, null));
-    List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before.size() + 1);
-    //app.logout();
-  }
+      List<GroupData> before = app.getGroupHelper().getGroupList();
+      GroupData group = new GroupData("Test 1", "Test header Cr1", "Test footer Cr1");
+      //app.getGroupHelper().createGroup(new GroupData("Test 1", "Test header 1", "Test footer 1"));
+      //app.getGroupHelper().createGroup(new GroupData("Test 1", null, null));
+      app.getGroupHelper().createGroup(group);
+      List<GroupData> after = app.getGroupHelper().getGroupList();
+      Assert.assertEquals(after.size(), before.size() + 1);
+
+      int max = 0;
+      for (GroupData g : after) {
+         if (g.getId() > max) {
+            max = g.getId();
+         }
+      }
+      group.setId(max);
+      before.add(group);
+      Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+      //app.logout();
+   }
 
 }
