4b55e63374a8659adbc064ad19951650f4faaef2
Черная Лариса <lchernaya@masterdm.ru>
2020-11-15 16:40:15 +0300

    Correction Contact tests before DZ_9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,15 +3,26 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.*;
 
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactCreationTests extends TestBase {
 
    @Test
    public void testContactCreation() throws Exception {
+      String groupName = "Test_С";
+      app.getNavigationHelper().gotoHomePage();
+      if (! app.getContactHelper().checkGroupList()) {
+         app.getNavigationHelper().gotoGroupPage();
+         app.getGroupHelper().createGroup(new GroupData(groupName, "TestHeader С", "TestFooter С"));
+         app.getNavigationHelper().gotoHomePage();
+      } else {
+         groupName = app.getContactHelper().getFirstGroupName();
+      }
+
       app.getContactHelper().initContactCreation();
       app.getContactHelper().fillContactForm(
            new ContactData("Антоний", "Васильевич", "Погорелов", "Domino", "Ant", "LTD", "Москва, ул.Нагорная 7", "8495555555555", "+7910555555555",
-                           "test@mail.ru", "7", "November", "1991", "Москва, Сосновая ул. 3-205", "Новый пользователь", "Test 1"), true);
+                           "test@mail.ru", "7", "November", "1991", "Москва, Сосновая ул. 3-205", "Новый пользователь", groupName), true);
       app.getContactHelper().submitContactCreation();
       app.getNavigationHelper().gotoHomePage();
    }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -13,14 +13,20 @@ import ru.stqa.pft.addressbook.model.GroupData;
 public class ContactDeletionTests extends TestBase {
    @Test
    public void testContactDeletion() {
+      String groupName = "Test_D";
       app.getNavigationHelper().gotoHomePage();
-      if (! app.getContactHelper().isThereAContact()) {
+      if (! app.getContactHelper().checkGroupList()) {
          app.getNavigationHelper().gotoGroupPage();
-         app.getGroupHelper().createGroup(new GroupData("Test_D", "TestHeader D", "TestFooter D"));
+         app.getGroupHelper().createGroup(new GroupData(groupName, "TestHeader D", "TestFooter D"));
          app.getNavigationHelper().gotoHomePage();
+      } else {
+         groupName = app.getContactHelper().getFirstGroupName();
+      }
+
+      if (! app.getContactHelper().isThereAContact()) {
          app.getContactHelper().createContact(new ContactData("Contact test d", "test d", "test d", null, null,
                                                               null, null, null, null, null, "5", "May",
-                                                              "2000", null, null, "Test_D"), true);
+                                                              "2000", null, null, groupName), true);
          app.getNavigationHelper().gotoHomePage();
       }
       app.getContactHelper().selectContact();
