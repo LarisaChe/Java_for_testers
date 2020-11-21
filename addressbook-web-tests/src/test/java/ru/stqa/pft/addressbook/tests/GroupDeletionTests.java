ec8ab006e238554046c855b355ff804071bf8898
Черная Лариса <lchernaya@masterdm.ru>
2020-11-21 17:06:02 +0300

    Лекция 5.2. Улучшение кода тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -3,27 +3,32 @@ package ru.stqa.pft.addressbook.tests;
 import java.util.List;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
-   @Test
-   public void testGroupDeletion() throws Exception {
+   @BeforeMethod
+   public void ensurePreconditions() {
       app.getNavigationHelper().gotoGroupPage();
-
-      List<GroupData> before = app.getGroupHelper().getGroupList();
       if (!app.getGroupHelper().isThereAGroup()) {
          app.getGroupHelper().createGroup(new GroupData("Test D", "Test header D", "Test footer D"));
       }
-      app.getGroupHelper().selectGroup(before.size() - 1);
+   }
+
+   @Test
+   public void testGroupDeletion() throws Exception {
+      List<GroupData> before = app.getGroupHelper().getGroupList();
+      int index = before.size() - 1;
+      app.getGroupHelper().selectGroup(index);
       app.getGroupHelper().deleteSelectedGroup();
       app.getGroupHelper().returnToGroupPage();
       List<GroupData> after = app.getGroupHelper().getGroupList();
-      Assert.assertEquals(after.size(), before.size() - 1);
+      Assert.assertEquals(after.size(), index);
 
-      before.remove(before.size() - 1);
+      before.remove(index);
       Assert.assertEquals(before, after);
    }
 
