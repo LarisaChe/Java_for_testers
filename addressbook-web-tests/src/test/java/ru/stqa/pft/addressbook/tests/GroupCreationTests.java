1f5e051d2f30d7c440d2ad0f1d21b33c45cb144d
Черная Лариса <lchernaya@masterdm.ru>
2020-11-25 23:06:41 +0300

    Лекция 5.8. Хеширование и предварительные проверки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -17,11 +17,21 @@ public class GroupCreationTests extends TestBase {
       Groups before = app.group().all();
       GroupData group = new GroupData().withName("Test Cr1").withHeader("Test header Cr1").withFooter("Test footer Cr1");
       app.group().create(group);
+      assertThat(app.group().count(), equalTo(before.size() + 1));
       Groups after = app.group().all();
-      assertThat(after.size(), equalTo(before.size() + 1));
-
       assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
       //app.logout();
    }
 
+   @Test
+   public void testBadGroupCreation() throws Exception {
+      app.goTo().groupPage();
+
+      Groups before = app.group().all();
+      GroupData group = new GroupData().withName("Te'st").withHeader("Test header Cr1").withFooter("Test footer Cr1");
+      app.group().create(group);
+      assertThat(app.group().count(), equalTo(before.size()));
+      Groups after = app.group().all();
+      assertThat(after, equalTo(before));
+   }
 }
