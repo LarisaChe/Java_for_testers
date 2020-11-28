62f8255f356ae22d29f7757d0bc01ff3f5d695cb
Черная Лариса <lchernaya@masterdm.ru>
2020-11-28 11:24:20 +0300

    Лекция 6.4.  (часть 1)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,11 @@ package ru.stqa.pft.addressbook.tests;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -10,12 +15,21 @@ import ru.stqa.pft.addressbook.model.Groups;
 
 public class GroupCreationTests extends TestBase {
 
-   @Test
-   public void testGroupCreation() throws Exception {
+   @DataProvider
+   public Iterator<Object[]> validGroups() {
+      List<Object[]> list = new ArrayList<Object[]>();
+      list.add(new Object[] {"test 'V1","header V1","footer V1"});
+      list.add(new Object[] {"test V2","header V2","footer V2"});
+      list.add(new Object[] {"test V3","header V3","footer V3"});
+      return list.iterator();
+   }
+
+   @Test (dataProvider = "validGroups")
+   public void testGroupCreation(String name, String header, String footer) throws Exception {
       app.goTo().groupPage();
 
       Groups before = app.group().all();
-      GroupData group = new GroupData().withName("Test Cr1").withHeader("Test header Cr1").withFooter("Test footer Cr1");
+      GroupData group = new GroupData().withName(name).withHeader(header).withFooter(footer);
       app.group().create(group);
       assertThat(app.group().count(), equalTo(before.size() + 1));
       Groups after = app.group().all();
