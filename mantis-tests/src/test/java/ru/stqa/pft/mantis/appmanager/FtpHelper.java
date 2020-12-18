91e73763dd28bae0c29b410a807d9d5a997bc883
Черная Лариса <lchernaya@masterdm.ru>
2020-12-18 12:41:52 +0300

    chmod - разрешения на доступ, актуально для MacOS
    ftpClient.sendSiteCommand("chmod 755 config_inc.php");

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/FtpHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/FtpHelper.java
@@ -18,10 +18,21 @@ public class FtpHelper {
    public void upload(File file, String target, String backup) throws IOException {
       ftp.connect(app.getProperty("ftp.host"));
       ftp.login(app.getProperty("ftp.login"), app.getProperty("ftp.password"));
+      /* из скайковского чата, инфа от Alexei :
+      относится к MacOS!!
+надо просто добавить права доступа
+chmod -R 755 <путь к папке с приложением>
+
+(можно и через Finder поменять, если в терминале неудобно)
+      а ещё лучше -- предоставьте права доступа
+chmod a+w /Applications/XAMPP/xamppfiles/htdocs/mantisbt-2.24.2/config/
+и ещё раз запустите инсталлятор, он сам создаст файл config_inc.php (сейчас он у вас не может этого сделать, потому что прав не хватает)
+      */
       ftp.deleteFile(backup);
       ftp.rename(target, backup);
       ftp.enterLocalPassiveMode();
       ftp.storeFile(target, new FileInputStream(file));
+      // ftpClient.sendSiteCommand("chmod 755 config_inc.php");
       ftp.disconnect();
    }
 
