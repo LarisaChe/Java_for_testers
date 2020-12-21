84553064a6879f1cfd568ae93f66e4cd6c839c62
Черная Лариса <lchernaya@masterdm.ru>
2020-12-21 18:54:37 +0300

    Лекция 9.5. RestAssured

---
+++ rest_sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
@@ -0,0 +1,60 @@
+package ru.stqa.pft.rest;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
+import ru.stqa.pft.model.Issue;
+
+public class RestAssuredTests {
+
+   @BeforeClass
+   public void init() {
+      RestAssured.authentication = RestAssured.basic("288f44776e7bec4bf44fdfeb1e646490", "");
+   }
+
+   @Test
+   public void testCreateIssue() throws IOException {
+      long now = System.currentTimeMillis();
+      // String.format("test %s ", now);
+      Set<Issue> oldIssues = getIssues();
+      Issue newIssue = new Issue().withSubject("Test issue L"+now).withDescription("Description test issue L"+now) ;
+      int issueId = createIssue(newIssue);
+      Set<Issue> newIssues = getIssues();
+      oldIssues.add(newIssue.withId(issueId));
+      System.out.println(newIssues.toString());
+      Assert.assertEquals(newIssues, oldIssues);
+   }
+
+   private Set<Issue> getIssues() throws IOException {
+      String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json").asString();
+           JsonElement parsed = new JsonParser().parse(json);
+      JsonElement issues = parsed.getAsJsonObject().get("issues");
+      return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {}.getType());
+   }
+
+   private int createIssue(Issue newIssue) throws IOException {
+      String json = RestAssured.given()
+                 .parameter("subject", newIssue.getSubject())
+                 .parameter("description", newIssue.getDescription())
+                 .post("https://bugify.stqa.ru/api/issues.json").asString();
+
+
+      JsonElement parsed = new JsonParser().parse(json);
+      System.out.println(parsed.toString());
+      System.out.println(parsed.getAsJsonObject().get("issue_id").getAsInt());
+      return parsed.getAsJsonObject().get("issue_id").getAsInt();
+   }
+
+}
