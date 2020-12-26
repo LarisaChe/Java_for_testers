9b1fb729776ce096ed821adcc4df8b813a194e8a
Черная Лариса <lchernaya@masterdm.ru>
2020-12-27 00:32:09 +0300

    Реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,14 +3,18 @@ package ru.stqa.pft.addressbook.appmanager;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 /**
  * Description.
@@ -38,13 +42,19 @@ public class ApplicationManager  {
       properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
       dbHelper = new DbHelper();
 
-      if (browser.equals(BrowserType.FIREFOX)) {
-        wd = new FirefoxDriver();
-     } else if (browser.equals(BrowserType.CHROME)) {
-        wd = new ChromeDriver();
-     } else if (browser.equals(BrowserType.IE)) {
-        wd = new InternetExplorerDriver();
-     }
+      if ("".equals(properties.getProperty("selenium.server"))) {
+         if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+         } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+         } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+         }
+      } else {
+         DesiredCapabilities capabilites = new DesiredCapabilities();
+         capabilites.setBrowserName(browser);
+         wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilites);
+      }
       wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
       //wd.get("http://localhost/addressbook/index.php");
       wd.get(properties.getProperty("web.baseUrl"));
