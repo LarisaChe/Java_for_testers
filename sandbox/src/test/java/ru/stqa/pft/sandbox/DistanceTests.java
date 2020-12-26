811f8269adf9fb1bb05a44446bdc95ca9fc27a21
Черная Лариса <lchernaya@masterdm.ru>
2020-12-26 23:31:17 +0300

    Тест исправлен

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
@@ -18,7 +18,7 @@ public class DistanceTests {
    public void testDistance2() {
       Point p1 = new Point(0, 0);
       Point p2 = new Point(0, 0);
-      Assert.assertEquals(p1.distance(p2), 10);
+      Assert.assertEquals(p1.distance(p2), 0);
    }
 
    @Test
